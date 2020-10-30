29d945fc0e3765f4e2eee6d82937d4ee4780c9fb
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-10-30 11:27:22 +0300

    Pushing from the same repository

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-WD=$(pwd)
-CLONE_DIR="${GITHUB_WORKSPACE}/website"
-git clone https://${GITHUB_TOKEN}@github.com/aerokube/aerokube.github.io.git ${CLONE_DIR}
-cd ${CLONE_DIR}
 git config user.name "${GITHUB_REPOSITORY}"
 git config user.email "aerokube@aerokube.github.com"
 git checkout master
-cp -R ${WD}/dist/* ${CLONE_DIR}
+cp -R dist/* .
 git add --all
-git commit -am "Publishing to gh-pages"
+git commit -am "Publishing to master"
 git push origin HEAD:master
