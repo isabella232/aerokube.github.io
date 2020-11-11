d413e171e18c36462e27273e383527c730a5aeda
Ivan Krutov <vania-pooh@vania-pooh.com>
2020-11-11 11:26:45 +0300

    Fixed deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+WD=$(pwd)
+CLONE_DIR="${GITHUB_WORKSPACE}/website"
+git clone -b gh-pages https://${GITHUB_TOKEN}@github.com/aerokube/aerokube.github.io.git ${CLONE_DIR}
+cd ${CLONE_DIR}
 git config user.name "${GITHUB_REPOSITORY}"
 git config user.email "aerokube@aerokube.github.com"
-git fetch origin
-git checkout gh-pages
-git rm main.*.css
-git rm main.*.js
-cp -R dist/* .
+git rm ${CLONE_DIR}/main.*.css
+git rm ${CLONE_DIR}/main.*.js
+cp -R ${WD}/dist/* ${CLONE_DIR}
 git add --all
-git commit -am "Publishing to gh-pages"
+git commit -am "Updated website"
 git push origin HEAD:gh-pages
